f34c29e85b056f200a0d691346bbda2a0e79a47d
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-01 09:43:54 +0530

    Add files via upload

---
+++ 4four.py
@@ -0,0 +1,8 @@
+# Day: 4 Our First Python Program 
+
+# Today we will write our first ever python program from scratch. It will consist of a bunch of print statements. print can be used to print something on the console in python
+
+print("Hello World")
+print(5)
+print("Python", 7)
+print(17*13)
