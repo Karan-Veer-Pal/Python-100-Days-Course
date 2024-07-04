8d5af1126119216799de476f8d10dde3f117c596
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-04 15:05:31 +0530

    Add files via upload

---
+++ 6six.py
@@ -0,0 +1,49 @@
+# Day: 6 Variables and Data Types 
+a = 1
+print(a)
+print("The type of a is ", type(a))
+print("\n")
+
+b = "Karan"
+print(b)
+print("The type of b is ", type(b))
+print("\n")
+
+c = 6.7777
+print(c)
+print("The type of c is ", type(c))
+print("\n")
+
+d = None
+print(d)
+print("The type of d is ", type(d))
+print("\n")
+
+e = True
+print(e)
+print("The type of e is ", type(e))
+print("\n")
+
+f = complex(8, 2)
+# print(e)
+print("The type of f is ", type(f))
+print("\n")
+
+print(a+c)
+print("\n")
+# print(a+b) Not Worked 
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print("This is a List:")
+print(list1)
+print("\n")
+
+tuple1 = (("parrot", "sparrow", "lion", "tiger"))
+print("This is a Tuple:")
+print(tuple1)
+print("\n")
+
+dict1 = {"name" : "karan", "age" : 20, "canVote" : True}
+print("This is a Dictionary:")
+print(dict1)
+print("\n")
