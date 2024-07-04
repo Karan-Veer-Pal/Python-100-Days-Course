7d5d11a67b7796682e30219540a25069c20a0f86
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-04 15:27:36 +0530

    Add files via upload

---
+++ 7seven.py
@@ -0,0 +1,29 @@
+# Day: 7 Exercise 1: Calculator using Python. By Me.
+
+# print(15+6)
+# print(15-6)
+# print(15*6)
+# print(15/6)
+# print(15//6)
+# print(15+6)
+# print(15%6)
+# print(2**3) # 3 is a power of 2.
+
+num1 = 15
+num2 = 6
+num3 = 2
+num4 = 3
+
+ans1 = num1 + num2
+ans2 = num1 - num2
+ans3 = num1 * num2
+ans4 = num1 / num2
+ans5 = num1 % num2
+ans6 = num3 ** num4
+
+print("Addition of", num1, "and", num2, "is", ans1)
+print("Subtraction of", num1, "and", num2, "is", ans2)
+print("Multiplication of", num1, "and", num2, "is", ans3)
+print("Division of", num1, "and", num2, "is", ans4)
+print("Modulo of", num1, "and", num2, "is", ans5)
+print("Power(Exponent) of", num3, "and", num4, "is", ans6)
