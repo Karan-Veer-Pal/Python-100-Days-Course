0d8ef9e2fee91a20dddf49bd1dc6052912ec4b64
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-07 07:37:14 +0530

    Add files via upload

---
+++ 10ten.py
@@ -0,0 +1,13 @@
+# Day : 10 Taking User Input in Python
+
+# print("Enter Your Name:")
+a = input("Enter Your Name:")
+print("My Name is", a)
+
+# input() function having a rule that is whatever you entered in x variable will be stored as a string.  
+x = input("Enter First Number: ")
+y = input("Enter Second Number: ")
+print("When Input Act like a string:")
+print(x + y)
+print("When Input Act like a Number:")
+print(int(x) + int(y))
