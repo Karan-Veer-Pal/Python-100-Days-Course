931973a41259c628d7e2bee9aadecca8821e95b3
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-09 10:54:34 +0530

    Update 12twelve.py

--- 12twelve.py
+++ 12twelve.py
@@ -1,9 +1,16 @@
 # Day : 12 String Slicing and Operation on Strings 
 # Slicing means print a string as per user requirement by using index number [0:5] --> 0 to 4 print
 
+# String Slicing & Operations on String
+# Length of a String
+# We can find the length of a string using len() function.
+
 names = "Karan,Veer"
 print(names[0:7]) # [0:5] --> Means where to where you want to print a string, this is known as slicing, ,this will return a Karan,V
 print(len(names)) # --> this will return a 10
+
+# String as an array
+# A string is essentially a sequence of characters also called an array. Thus we can access the elements of this array.
 
 # Eg. --> To finding a length of a string
 fruit = "Mango"
@@ -19,6 +26,10 @@ print(fruit[-1:-3]) # --> this will return a 5
 print(fruit[-3:-1]) # --> this will return a ng, 5-3:5-1 = 2:4
 
 # Loop through a String:
+
+# Loop through a String:
+# Strings are arrays and arrays are iterable. Thus we can loop through strings.
+
 alphabets = "ABCDE"
 for i in alphabets:
     print(i)# --> this will return a A, B, C, D, E  
