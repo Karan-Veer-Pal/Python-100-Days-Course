a5cacc6bb7f6f0bc14647d254d30eb3bc2fe4842
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-13 16:56:47 +0530

    Update 16sixteen.py

--- 16sixteen.py
+++ 16sixteen.py
@@ -1,5 +1,23 @@
 # Day : 16 Match Case Statement 
 
+# To implement switch-case like characteristics very similar to if-else functionality, we use a match case in python. If you are coming from a C, C++ or Java like language, you must have heard of switch-case statements. If this is your first language, dont worry as I will tell you everything you need to know about match case statements in this video!
+
+# A match statement will compare a given variable’s value to different shapes, also referred to as the pattern. The main idea is to keep on comparing the variable with all the present patterns until it fits into one.
+
+# The match case consists of three main entities :
+
+# The match keyword
+# One or more case clauses
+# Expression for each case
+# The case clause consists of a pattern to be matched to the variable, a condition to be evaluated if the pattern matches, and a set of statements to be executed if the pattern matches.
+
+# Syntax:
+# match variable_name:
+#             case ‘pattern1’ : //statement1
+#             case ‘pattern2’ : //statement2
+#             …            
+#             case ‘pattern n’ : //statement n
+
 # not use break-keyword in this language
 
 # to check which version is used 
