66c5f4cb1c1107876fdaa47723880e1eed624dfc
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-06 19:25:27 +0530

    Add files via upload

---
+++ 39thirtynine.py
@@ -0,0 +1,128 @@
+# Day : 39 Exercise : 3 Kaun Banega Crorepati (KBC) Solution : By Tutor
+
+# Question : Create a program capable of displaying questions to the user like KBC. Use List data type to store the questions and their correct answers. Display the final amount the person is taking home after playing the game.
+
+questions = [
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+  [
+    "Which language was used to create fb?", "Python", "French", "JavaScript",
+    "Php", "None", 4
+  ],
+]
+
+levels = [1000, 2000, 3000, 5000, 10000, 20000, 40000, 80000, 160000, 320000]
+money = 0
+for i in range(0, len(questions)):
+
+  question = questions[i]
+  print(f"\n\nQuestion for Rs. {levels[i]}")
+  print(f"a. {question[1]}          b. {question[2]} ")
+  print(f"c. {question[3]}          d. {question[4]} ")
+  reply = int(input("Enter your answer (1-4) or  0 to quit:\n" ))
+  if (reply == 0):
+    money = levels[i-1]
+    break
+  if(reply == question[-1]):
+    print(f"Correct answer, you have won Rs. {levels[i]}")
+    if(i == 4):
+      money = 10000
+    elif(i == 9):
+      money = 320000
+    elif(i == 14):
+      money = 10000000
+  else:
+    print("Wrong answer!")
+    break 
+
+print(f"Your take home money is {money}")
