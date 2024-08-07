0413327ae34cbc64e0de5549df13871c547da897
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-07 11:31:51 +0530

    Add files via upload

---
+++ 40forty.py
@@ -0,0 +1,21 @@
+# Day : 40 Exercise : 4 Secret Code Language Solution By Me :
+
+# Write a python program to translate a message into secret code language. Use the rules below to translate normal English into secret code language
+
+# Coding:
+# if the word contains atleast 3 characters, remove the first letter and append it at the end
+#   now append three random characters at the starting and the end
+# else:
+#   simply reverse the string
+
+# Decoding:
+# if the word contains less than 3 characters, reverse it
+# else:
+#   remove 3 random characters from start and end. Now remove the last letter and append it to the beginning
+
+# Your program should ask whether you want to code or decode
+
+message = input("Enter your message:")
+
+message[0] = new_message([])
+new_message = message.append(message[0])
